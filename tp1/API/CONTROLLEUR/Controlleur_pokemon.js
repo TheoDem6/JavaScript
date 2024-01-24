2c76759ebea2e68acfa6caa27efa3ba734af36ca
TheoDem6 <theo.demany@gmail.com>
2024-01-24 12:28:36 +0100

    cest trop bien

--- tp1/API/CONTROLLEUR/Controlleur_pokemon.js
+++ tp1/API/CONTROLLEUR/Controlleur_pokemon.js
@@ -7,10 +7,24 @@ router.get('/API/V1/POKEMON', async (req, res) => {
   res.json(pokemons);
 });
 router.get('/API/V1/POKEMON/search', async (req, res) => {
-    const hpValue = parseInt(req.query);  
-    console.log(req.query.hp);
-  
-    const pokemon = PokemonService.searchByHP(hpValue);
+    const hpValue = parseInt(req.query.hp);  
+    const typeValue = req.query.type;  
+    const nomValue = req.query.nom; 
+    console.log("test",typeValue);
+    console.log(req.query);
+    
+    let pokemon = [];
+
+    if(!isNaN(hpValue)){
+        console.log("marche stp 1");
+         pokemon = PokemonService.searchByHP(hpValue);
+    }else if (typeValue != undefined){
+        console.log("marche stp 2");
+         pokemon = PokemonService.searchByType(typeValue);
+    }else{
+        console.log("marche stp 3 ");
+         pokemon = PokemonService.searchByName(nomValue);
+    }
   
     res.json(pokemon);
   });
--- tp1/Service/service_pokemon.js
+++ tp1/Service/service_pokemon.js
@@ -25,9 +25,39 @@ function  update(id, pokemonData) {
     return Pokemon.findByIdAndUpdate(id, pokemonData, { new: true });
 }
 function searchByHP(hpValue) {
-    //console.log(hpValue);
-    const matchingPokemon = pokemonArray.filter(pokemon => pokemon.base.HP === hpValue);
-    return matchingPokemon;
+    const tabPokemon = [];
+    for (let i  = 0; i < Pokemon.length;i++){
+        if (Pokemon[i].base.HP == hpValue){
+            tabPokemon.push(Pokemon[i]);
+        }
+    }
+    
+    return tabPokemon;
   }
 
-module.exports = { getAll,getById,create,update,searchByHP};
+  function searchByName(nomValue) {
+    const tabPokemon = [];
+    console.log(nomValue);
+    for (let i  = 0; i < Pokemon.length;i++){
+        if (Pokemon[i].name.french == nomValue){
+            tabPokemon.push(Pokemon[i]);
+        }
+    }
+    
+    return tabPokemon;
+  }
+  function searchByType(typeValue) {
+    const tabPokemon = [];
+    console.log(typeValue);
+    for (let i  = 0; i < Pokemon.length;i++){
+        for (let j = 0; j<Pokemon[i].type.length;j++){
+            if(Pokemon[i].type[j] == typeValue){
+                tabPokemon.push(Pokemon[i]);
+            }
+        }
+    }
+    
+    return tabPokemon;
+  }
+
+module.exports = { getAll,getById,create,update,searchByHP,searchByName,searchByType};
